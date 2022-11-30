a46f82d00c22424a3c4dac860d6bf362106a644d
Yuli Tamara <79260463+Yulitamara@users.noreply.github.com>
2022-11-30 15:50:01 +0200

    s

--- up/js/scroll.js
+++ up/js/scroll.js
@@ -1,7 +1,9 @@
 const scroll = document.querySelector(".scroll");
+const bgNav = document.querySelector(".header__nav");
 
 window.addEventListener("scroll", () => {
   scroll.classList.toggle("active", window.scrollY > 200);
+  bgNav.style.backgroundColor = "#1a1c1ded";
 });
 
 function scrollToTop() {
