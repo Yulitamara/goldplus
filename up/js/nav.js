98e16adea607397d777f81754b11f398954c8edd
Yuli Tamara <79260463+Yulitamara@users.noreply.github.com>
2022-11-30 14:38:30 +0200

    goldplus

---
+++ up/js/nav.js
@@ -0,0 +1,13 @@
+const header = document.querySelector(".header");
+const menu = document.querySelector(".header__menu");
+const overlay = document.querySelector(".header__overlay");
+
+menu.addEventListener("click", () => {
+  menu.classList.toggle("open");
+  header.classList.toggle("open");
+  overlay.classList.toggle("open");
+});
+
+// Footer //
+
+document.getElementById("year").innerHTML = new Date().getFullYear();
